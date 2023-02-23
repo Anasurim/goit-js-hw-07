422fb9675a66738e3f580f71009e56b07e4bd520
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-02-23 01:18:53 +0100

    DOM to practice, homework 07: completed gallery task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<div class="gallery__item"><a class="gallery__link" href="${original}" "><img
+            class="gallery__image"
+             src="${preview}"
+             data-source="${original}"
+             alt="${description}"
+             style="cursor: pointer"
+      />
+</a></div>`
+);
+
+galleryRef.insertAdjacentHTML("afterbegin", galleryMarkup.join(""));
+
+galleryRef.onclick = (e) => {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+
+  basicLightbox
+    .create(
+      `<img
+            class="gallery__image"
+            src="${e.target.dataset.source}"
+        />`
+    )
+    .show();
+};
+
+// galleryRef.addEventListener("mouseenter", (e) => {
+//   console.log(e.currentTarget);
+//   e.currentTarget.style.cursor = "pointer";
+// });
